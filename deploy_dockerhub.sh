050245b5182dbd2d4a2ac466d59bd1d67585880b
Mrinal Bhattacharya <Mrinal.bhattacharya@outlook.com>
2019-04-08 16:02:23 +0530

    Update deploy_dockerhub.sh

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,9 +1,6 @@
-docker login –u=mrinalbhattacharya –p=Docker@1987 docker.io
-docker build -f Dockerfile -t restful-api-springboot-docker .
-docker tag restful-api-springboot-docker mrinalbhattacharya/restful-api-springboot-docker
-echo "build and tag is done"
-docker images
-docker container ls
-#docker tag $DOCKER_REPO:latest $DOCKER_REPO:$TAG
-
-docker push mrinalbhattacharya/restful-api-springboot-docker
+- docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+- docker build -t mrinalbhattacharya/restful-api-springboot-docker:latest .
+- echo "build and tag is done"
+- docker images
+- docker container ls
+- docker push mrinalbhattacharya/restful-api-springboot-docker:latest
