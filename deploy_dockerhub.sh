992625f88343debd965a676c53f445d7e4a64e71
Mrinal Bhattacharya <Mrinal.bhattacharya@outlook.com>
2019-04-08 12:17:38 +0530

    Update deploy_dockerhub.sh

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,4 +1,4 @@
-docker login –u "mrinalbhattacharya" –p "Docker@1987"
+docker login –u=mrinalbhattacharya –p=Docker@1987
 docker build -f Dockerfile -t mrinalbhattacharya/restful-api-springboot-docker:latest .
 
 #docker tag $DOCKER_REPO:latest $DOCKER_REPO:$TAG
