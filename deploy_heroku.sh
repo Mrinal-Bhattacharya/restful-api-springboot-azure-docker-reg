47840acc09a2c4b0c05d26b1cd0f3297b63b695c
Mrinal Bhattacharya <Mrinal.bhattacharya@outlook.com>
2019-04-10 14:49:55 +0530

    Update deploy_heroku.sh

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -2,7 +2,7 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 heroku plugins:install @heroku-cli/plugin-container-registry
 
-docker login –username _ –password=$HEROKU_API_KEY registry.heroku.com
+docker login –u _ –p "$HEROKU_API_KEY" registry.heroku.com
 
 heroku container:push web –app restful-api-springboot-docker
 
